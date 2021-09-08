c9b76798352bb16fd33df92e9235bc6e09092711
shevchenko-ivan <shevchenkoivan1951@gmail.com>
2021-09-09 02:53:44 +0300

    yarn st

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './components/App'
-import reportWebVitals from './reportWebVitals'
-
-import './index.css'
-import './assets/style/colors.css'
-import './assets/style/fonts.css'
-import './assets/style/bootstrap-grid.css'
-import 'react-phone-number-input/style.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
